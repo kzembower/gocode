fafc3f2b95da41e2c648810610c79dd5f8ff0190
Kevin Zembower <kzembower@verizon.net>
2014-01-18 17:07:34 -0500

    Version 4, with newmath library.

--- hello.go
+++ hello.go
@@ -1,9 +1,11 @@
 package main
 
 import "fmt"
+import "github.com/kzembower/newmath"
 
 func main() {
-    fmt.Printf("hello, world\n")
-    fmt.Printf("version 3 at 1655\n")
+	fmt.Printf("hello, world\n")
+	fmt.Printf("version 4 at 1707\n")
+	fmt.Printf("Sqrt(2) = %v\n", newmath.Sqrt(2))
 }
 
